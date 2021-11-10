25e8414e536efef33270bde7832bb00146b2a785
h586636 <54099844+h586636@users.noreply.github.com>
2021-11-10 14:16:46 +0100

    Update boxofficeapp.py

--- src/boxofficeapp.py
+++ src/boxofficeapp.py
@@ -1,6 +1,7 @@
 from pycaret.regression import load_model, predict_model 
 import pandas as pd 
 import numpy as np
+from pathlib import Path
 import streamlit as st
 from PIL import Image
 import os
